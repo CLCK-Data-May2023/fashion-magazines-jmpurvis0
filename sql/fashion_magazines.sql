a06399c236e5cbe6dc43edc3dfbae37dc9d01ead
jmpurvis0 <jmpurvis0@gmail.com>
2023-10-21 18:26:32 -0400

    wrote sql query; edited .gitignore

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,10 @@
--- Add your sql here
+SELECT c.customer_name, SUM(unpaid.due)
+FROM customers c JOIN
+(SELECT o.customer_id,
+s.price_per_month*s.subscription_length AS due,
+o.order_status
+FROM orders o join subscriptions s
+ON o.order_status = "unpaid"
+AND description = "Fashion Magazine") unpaid
+ON c.customer_id = unpaid.customer_id
+GROUP BY c.customer_name
