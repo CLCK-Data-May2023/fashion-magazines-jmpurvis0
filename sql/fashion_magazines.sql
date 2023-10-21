df6474bfd00def2b56011ca79a933ecf63cd6167
jmpurvis0 <jmpurvis0@gmail.com>
2023-10-21 18:38:41 -0400

    update sql query

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -3,8 +3,9 @@ FROM customers c JOIN
 (SELECT o.customer_id,
 s.price_per_month*s.subscription_length AS due,
 o.order_status
-FROM orders o join subscriptions s
-ON o.order_status = "unpaid"
+FROM orders o JOIN subscriptions s
+ON o.subscription_id = s.subscription_id
+WHERE o.order_status = "unpaid"
 AND description = "Fashion Magazine") unpaid
 ON c.customer_id = unpaid.customer_id
 GROUP BY c.customer_name
